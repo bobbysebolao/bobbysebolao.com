eaa419f75a620a4eb5cd065bd5158c617c56b04d
Bobby <bobbysebolao@gmail.com>
2019-04-06 23:17:33 +0100

    Complete blog home layout for default and sprite mode

--- public/blog/recentposts.js
+++ public/blog/recentposts.js
@@ -131,3 +131,45 @@ else {
   standardStylesheet.href = "../css/blog.css";
 }
   });
+
+
+
+
+  let typed = document.querySelector(".landing-screen__intro--typed");
+  console.log(typed.textContent.length);
+  let words = ["( Financially Free )", "( Creative Dreamers )", "( Daring Designers )", "( Master Craftspeople )", "( Amateur Builders )", "( City Dwellers )", "( Nature Lovers )", "( Global Citizens )", "( Intrepid Travellers )", "( Tiny Housers )"];
+
+  let delay = 40;
+  let baseline = 0;
+  let count = 1;
+
+  for (let i = 0; i < words.length; i++) {
+    let help = 0;
+
+    for (let j = 0; j < words[i].length; j++) {
+
+      (function () {
+        setTimeout(function () {
+        typed.textContent += words[i][j];
+      }, baseline+(delay*j));
+      help = j;
+
+    })();
+
+  }
+    baseline += (delay*words[i].length)*4;
+
+    for (let p = words[i].length-1; p>=0; p--) {
+
+      (function () {
+        setTimeout(function () {
+
+          typed.textContent = typed.textContent.slice(0,typed.textContent.indexOf(typed.textContent.length-1));
+
+      }, baseline+(delay*help));
+      help++;
+
+    })();
+  };
+    baseline += (delay*words[i].length)*2;
+    }
