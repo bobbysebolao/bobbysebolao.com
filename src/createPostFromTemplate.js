3dca478722faf501d5d484b6dd020c17bd2301c8
Bobby <bobbysebolao@gmail.com>
2019-04-09 22:09:01 +0100

    Flesh out post-template.html, split recentposts.js into a separate function for animated intro text

--- src/createPostFromTemplate.js
+++ src/createPostFromTemplate.js
@@ -2,17 +2,19 @@ const fs = require("fs");
 const querystring = require("querystring");
 const path = require("path");
 
-const createPostFromTemplate = function(title, body, date, readingTime, mainImage, metatitle, metadescription) {
+const createPostFromTemplate = function(title, subtitle, body, date, readingTime, mainImage, mainImageAltText, mainImageCaption, metatitle, metadescription) {
 
   let data = fs.readFileSync(__dirname + "/../public/blog/post-template.html", "utf8")
 
     data = data.replace("*****insertposttitlehere*****", title);
+    data = data.replace("*****insertpostsubtitlehere*****", subtitle);
     data = data.replace("*****insertpostbodyhere*****", body);
     data = data.replace("*****insertpostdatehere*****", date.split(" ").slice(0,4).join(" "));
     data = data.replace("*****insertreadingtimehere*****", readingTime);
     data = data.replace("*****insertpostmainimagehere*****", "/../assets/images/blog/" + mainImage);
+    data = data.replace("*****insertmainimagealttexthere*****", mainImageAltText);
+    data = data.replace("*****insertmainimagecaption*****", mainImageCaption);
     data = data.replace("*****insertpostmetatitlehere*****", metatitle);
-    data = data.replace("*****insertpostmetadescriptionhere*****", metadescription);
     data = data.replace("*****insertpostmetadescriptionhere*****", metadescription);
     return data;
     // createdPost = data;
--- src/readingTimeCalculator.js
+++ src/readingTimeCalculator.js
@@ -1,9 +1,5 @@
-const fs = require("fs");
-const querystring = require("querystring");
-const path = require("path");
-
 const readingTimeCalculator = function(body) {
-  return Math.floor((body.split(" ").length/4)/60);
+  return Math.round((body.split(" ").length/4)/60);
 }
 
 module.exports = readingTimeCalculator;
