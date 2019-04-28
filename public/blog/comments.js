70709d82afe05b846b686f35d30ac88836c2dae9
Bobby <bobbysebolao@gmail.com>
2019-04-28 21:51:15 +0100

    Style blog post comments section

--- public/blog/comments.js
+++ public/blog/comments.js
@@ -12,23 +12,36 @@ document.onreadystatechange = function() {
         for (let comment in data) {
           console.log("MEMEME", data[comment]);
           let commentContainer = document.createElement("div");
+          let userContainer = document.createElement("div");
+          let usernameContainer = document.createElement("div");
           let userAvatar = document.createElement("img");
           let commentUsername = document.createElement("p");
           let commentDate = document.createElement("p");
           let commentBody = document.createElement("p");
           commentContainer.className = "user-comments__comment";
+          userContainer.className = "user-comments__user";
+          usernameContainer.className = "user-comments__username";
+
           userAvatar.className = "user-comments__avatar";
           commentUsername.className = "user-comments__username";
           commentDate.className = "user-comments__date";
+
           commentBody.className = "user-comments__body";
 
           userAvatar.src = "../assets/images/users/" + data[comment]["avatar_name"];
           commentUsername.textContent = data[comment]["username"];
           commentDate.textContent = data[comment]["com_date"].split(" ").slice(1, 4).join(" ");
           commentBody.textContent = data[comment]["body"];
-          commentContainer.appendChild(userAvatar);
-          commentContainer.appendChild(commentUsername);
-          commentContainer.appendChild(commentDate);
+          // userContainer.appendChild(userAvatar);
+          usernameContainer.appendChild(commentUsername);
+          usernameContainer.appendChild(commentDate);
+          userContainer.appendChild(userAvatar);
+          userContainer.appendChild(usernameContainer);
+          // commentContainer.appendChild(userAvatar);
+          // commentContainer.appendChild(commentUsername);
+          // commentContainer.appendChild(commentDate);
+          commentContainer.appendChild(userContainer);
+          // commentContainer.appendChild(usernameContainer);
           commentContainer.appendChild(commentBody);
           userComments.appendChild(commentContainer);
         }
