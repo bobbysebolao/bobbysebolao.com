de790756f98e4cd80bdcf2433a38be67cb5d2302
Bobby <bobbysebolao@gmail.com>
2019-06-26 23:55:36 +0100

    Display airtable images in project divs on home page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,16 +1,23 @@
-const portfolioProjects = document.getElementsByClassName("portfolio-project");
-console.log(portfolioProjects, "Hi");
+const projectsContainer = document.querySelector(".projects-container");
+const projectsHTMLCollection = document.getElementsByClassName(
+  "portfolio-project"
+);
+// console.log(projectsHTMLCollection, "Hi");
+
+let projectsArr = Array.prototype.slice.call(projectsHTMLCollection);
+// console.log(projectsArr.length, "Yes");
 
 fetch("/projects")
   .then(res => res.json())
-  .then(userData => {
-    console.log("Here are the airtable results: ", userData);
-    return;
-    if (userData.loginStatus !== true) {
-      userCommentsForm.style.display = "none";
-      loginToComment.style.display = "block";
-    } else {
-      userCommentsForm.style.display = "block";
-      loginToComment.style.display = "none";
+  .then(projectsData => {
+    console.log("Here are the airtable results: ", projectsData);
+    for (let i = 0; i < projectsData.length; i++) {
+      let newProject = document.createElement("div");
+      newProject.className = "portfolio-project";
+      projectsContainer.appendChild(newProject);
+      // console.log("The image URL: ", projectsData[i]["Image"][0]["url"]);
+      projectsContainer.style.backgroundImage = `url("${
+        projectsData[i]["Image"][0]["url"]
+      }")`;
     }
   });
