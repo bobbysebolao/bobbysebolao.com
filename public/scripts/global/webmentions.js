ba3cc74d11beec5c5864b673bfc43d60775961d4
Bobby <bobbysebolao@gmail.com>
2019-07-24 19:34:32 +0000

    Change webmentions.js fetch request url

--- public/scripts/global/webmentions.js
+++ public/scripts/global/webmentions.js
@@ -3,7 +3,8 @@ const thisUrl = window.location;
 const postUrl = thisUrl.protocol + "//" + thisUrl.host + thisUrl.pathname
 console.log("SKRAAA", postUrl);
 
-fetch(`https://webmention.io/api/mentions.jf2?target=${postUrl}`)
+// fetch(`https://webmention.io/api/mentions.jf2?target=${postUrl}`)
+fetch(`https://webmention.io/api/mentions.html?token=e-8Tn3bFTJK5YgvMOXXctA`)
 .then(res => {
   console.log("The webmentions data: ", res)
 })
