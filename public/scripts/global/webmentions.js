6f599b6d4dae5010ef198ed3ed29f81a95d92116
Bobby <bobbysebolao@gmail.com>
2019-07-24 18:09:27 +0100

    Fix postUrl in webmentions.js

--- public/scripts/global/webmentions.js
+++ public/scripts/global/webmentions.js
@@ -3,7 +3,7 @@ const thisUrl = window.location;
 const postUrl = thisUrl.protocol + "//" + thisUrl.host + thisUrl.pathname
 console.log("SKRAAA", postUrl);
 
-fetch(`${postUrl}`)
+fetch(`https://webmention.io/api/mentions.jf2?target=${postUrl}`)
 .then(res => {
   console.log("The webmentions data: ", res)
 })
