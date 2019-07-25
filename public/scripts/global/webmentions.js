c1efc19685a7c224dd59fb1a5f60c4661e0c958e
Bobby <bobbysebolao@gmail.com>
2019-07-25 22:06:27 +0100

    Edit webmentions.js

--- public/scripts/global/webmentions.js
+++ public/scripts/global/webmentions.js
@@ -12,21 +12,18 @@ fetch(`https://webmention.io/api/mentions.jf2?target=${postUrl}`)
 .then(res => res.json())
 .then(data => {
   let webmentions = data['children'];
-  // console.log("The webmentions response object: ", data);
+  console.log("The webmentions response object: ", data);
   // console.log("The type of webmention: ", data['children'][0]['wm-property']);
 
   for (let i = 0; i < webmentions.length; i++) {
     if (webmentions[i]['wm-property'] === "like-of") {
       likes.push(webmentions[i]);
-      console.log("LIKES:", likes);
     } else if (webmentions[i]['wm-property'] === "repost-of") {
       reposts.push(webmentions[i]);
     } else if (webmentions[i]['wm-property'] === "in-reply-to") {
       replies.push(webmentions[i]);
     }
   }
-})
-.then(end => {
   console.log("LIKES:", likes);
   console.log("REPOSTS:", reposts);
   console.log("REPLIES:", replies);
