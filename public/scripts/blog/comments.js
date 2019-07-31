43c47fb71a6ce2e2f1db65af233d26e780d81a89
Bobby <bobbysebolao@gmail.com>
2019-07-31 12:12:04 +0100

    Attempt fix async fetch request issue for fetching webmentions 21

--- public/scripts/blog/comments.js
+++ public/scripts/blog/comments.js
@@ -115,6 +115,77 @@ fetch("/blog/comments")
     .then(unrelated2 => {
       console.log("FINAL TALLY OF COMMENTS AND WEBMENTIONS", data);
     })
+    .then(unrelated3 => {
+      if (data) {
+        for (let comment in data) {
+          console.log("MEMEME", data[comment]);
+          let commentContainer = document.createElement("div");
+          let userContainer = document.createElement("div");
+          let usernameContainer = document.createElement("div");
+          let userAvatar = document.createElement("img");
+          let commentUsername = document.createElement("p");
+          let commentDate = document.createElement("p");
+          let commentBody = document.createElement("p");
+          commentContainer.className = "user-comments__comment";
+          userContainer.className = "user-comments__user";
+          usernameContainer.className = "user-comments__username";
+
+          userAvatar.className = "blog-post__user-avatar";
+          commentUsername.className = "user-comments__username";
+          commentDate.className = "user-comments__date";
+
+          commentBody.className = "user-comments__body";
+
+          // userAvatar.src = `https://s3.eu-west-2.amazonaws.com/console-blog/user-avatars/${data[comment]["avatar_name"].split(".")[0]}-user-image.${data[comment]["avatar_name"].split(".")[1]}`;
+          userAvatar.src = `${data[comment]["avatar_filepath"]}`;
+          // https://s3.eu-west-2.amazonaws.com/console-blog/user-avatars/
+          // `https://s3.eu-west-2.amazonaws.com/console-blog/blog-images/${data[comment]["avatar_name".split(".")[0]}-user-image.${data[comment]["avatar_name".split(".")[1]}`
+          commentUsername.textContent = data[comment]["username"];
+
+          console.log("GOOOOOOAAAAALASSO", data[comment]["com_date"].split(" "));
+
+          if (data[comment]["com_date"].split(" ").length === 1) {
+            commentDate.textContent = twitterDateConverter(data[comment]["com_date"]
+              .split("-"))
+          } else {
+            commentDate.textContent = data[comment]["com_date"]
+              .split(" ")
+              .slice(1, 4)
+              .join(" ");
+          }
+          commentBody.textContent = data[comment]["body"];
+          // userContainer.appendChild(userAvatar);
+
+          if (data[comment]["link"]) {
+            let userAvatarLink = document.createElement("a");
+            userAvatarLink.href = `${data[comment]["link"]}`;
+            let commentUsernameLink = document.createElement("a");
+            commentUsernameLink.href = `${data[comment]["link"]}`;
+
+            userAvatarLink.appendChild(userAvatar);
+            commentUsernameLink.appendChild(commentUsername);
+            userContainer.appendChild(userAvatarLink);
+            usernameContainer.appendChild(commentUsernameLink);
+            userContainer.appendChild(userAvatarLink);
+          } else {
+            usernameContainer.appendChild(commentUsername);
+            userContainer.appendChild(userAvatar);
+          }
+
+            usernameContainer.appendChild(commentDate);
+            // userContainer.appendChild(userAvatar);
+            userContainer.appendChild(usernameContainer);
+            // commentContainer.appendChild(userAvatar);
+            // commentContainer.appendChild(commentUsername);
+            // commentContainer.appendChild(commentDate);
+            commentContainer.appendChild(userContainer);
+            // commentContainer.appendChild(usernameContainer);
+            commentContainer.appendChild(commentBody);
+            userComments.appendChild(commentContainer);
+
+        }
+      }
+    })
 })
 
 
