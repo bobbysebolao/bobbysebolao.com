d93e48fc734e130abbc4245d6e89f4a11684b7a4
Bobby <bobbysebolao@gmail.com>
2019-09-12 16:38:40 +0300

    Publish blog post 6

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -237,6 +237,9 @@ VALUES ('1565938630239', 'Fri Aug 16 2019 06:57:10 GMT+0000 (Coordinated Univers
 INSERT INTO posts (pub_timestamp, pub_date, title, subtitle, reading_mins, main_image_caption, main_image_alt_text, filename, filepath, category, tags, main_image_id, thumbnail_id, user_id)
 VALUES ('1566643456873', 'Sat Aug 24 2019 10:44:16 GMT+0000 (Coordinated Universal Time)', 'My Blogging Anniversary', 'Ten years of writing on the web!', 5, 'Image © 2019 Vectorstory', 'A speech bubble with the word "Blog", surrounded by icons representing creativity', 'my-blogging-story.html', 'https://s3.eu-west-2.amazonaws.com/console-blog/blog-posts/my-blogging-story.html', 'life', 'update', 17, 5, 1);
 
+INSERT INTO posts (pub_timestamp, pub_date, title, subtitle, reading_mins, main_image_caption, main_image_alt_text, filename, filepath, category, tags, main_image_id, thumbnail_id, user_id)
+VALUES ('1568294838231', 'Thu Sep 12 2019 13:27:18 GMT+0000 (Coordinated Universal Time)', '3 Reasons to Build in Black and White', 'There is a method to the monochrome...', 3, 'Image © 2019 Disney', 'Old black and white Mickey Mouse cartoon', 'why-build-in-black-and-white.html', 'https://s3.eu-west-2.amazonaws.com/console-blog/blog-posts/why-build-in-black-and-white.html', 'learn', 'design-patterns', 19, 6, 1);
+
 INSERT INTO main_images (name, size, filepath, type)
 VALUES ('jamstack-2019-640x360px.jpg', 83722, 'https://console-blog.s3.amazonaws.com/blog-images/jamstack-2019-640x360px-main-image.jpg', 'image/jpeg');
 
@@ -285,6 +288,15 @@ VALUES ('falafel-640x360px.jpg', 27250, 'https://console-blog.s3.amazonaws.com/b
 INSERT INTO main_images (name, size, filepath, type)
 VALUES ('blogging-story-640x360px.jpg', 51409, 'https://console-blog.s3.amazonaws.com/blog-images/blogging-story-640x360px-main-image.jpg', 'image/jpeg');
 
+INSERT INTO main_images (name, size, filepath, type)
+VALUES ('color-in-data-viz-640x360px.jpg', 32399, 'https://console-blog.s3.amazonaws.com/blog-images/color-in-data-viz-640x360px-main-image.jpg', 'image/jpeg');
+
+INSERT INTO main_images (name, size, filepath, type)
+VALUES ('mango-metrics-monochrome-327x596px.png', 12139, 'https://console-blog.s3.eu-west-2.amazonaws.com/blog-images/mango-metrics-monochrome-327x596px-main-image.png', 'image/png');
+
+INSERT INTO main_images (name, size, filepath, type)
+VALUES ('mickey-mouse-black-and-white-640x360px.jpg', 55044, 'https://console-blog.s3.eu-west-2.amazonaws.com/blog-images/mickey-mouse-black-and-white-640x360px-main-image.jpg', 'image/jpeg');
+
 INSERT INTO thumbnails (name, size, filepath, type)
 VALUES ('jamstack-2019-210x118px.jpg', 6341, 'https://console-blog.s3.amazonaws.com/blog-thumbnails/jamstack-2019-210x118px-thumbnail-image.jpg', 'image/jpeg');
 
@@ -299,6 +311,9 @@ VALUES ('falafel-210x118px.jpg', 5012, 'https://console-blog.s3.amazonaws.com/bl
 
 INSERT INTO thumbnails (name, size, filepath, type)
 VALUES ('blogging-story-210x118px.jpg', 10172, 'https://console-blog.s3.amazonaws.com/blog-thumbnails/blogging-story-210x118px-thumbnail-image.jpg', 'image/jpeg');
+
+INSERT INTO thumbnails (name, size, filepath, type)
+VALUES ('mickey-mouse-black-and-white-210x118px.jpg', 10181, 'https://console-blog.s3.amazonaws.com/blog-thumbnails/mickey-mouse-black-and-white-210x118px-thumbnail-image.jpg', 'image/jpeg');
 
 INSERT INTO users (first_name, last_name, username, email, password, role, avatar_name, avatar_size, avatar_filepath, avatar_type, is_verified)
 VALUES ('Bobby', 'Sebolao', 'mistapepper', 'bobbysebolao@gmail.com', '$2a$10$6ASiU.Ue2fmKtzD5QLaNfOH4D6J3sP/EIRF1/UhO/NsDODJQLGp7G', 'admin', 'bobby-250x250px-user-image.jpg', 22052, 'https://console-blog.s3.amazonaws.com/user-avatars/bobby-250x250px-user-image.jpg', 'image/jpeg', 'True');
