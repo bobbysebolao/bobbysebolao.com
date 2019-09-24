709ca2e31cf0fa8ef115de5a19ab5e06e5d3f46a
Bobby <bobbysebolao@gmail.com>
2019-09-24 21:46:54 +0300

    Write passing test for readingTimeCalculator function

--- src/__tests__/helpers.test.js
+++ src/__tests__/helpers.test.js
@@ -1,4 +1,10 @@
+const readingTimeCalculator = require("../readingTimeCalculator.js")
+
 // This test fails because 1 !== 2
 it('Testing to see if Jest works', () => {
     expect(2).toBe(2)
   })
+
+  test('Calculates avg time to read a chunk of text', () => {
+    expect(readingTimeCalculator("how now brown cow")).toBe(0);
+  });
