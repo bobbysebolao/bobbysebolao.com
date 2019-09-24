abc6a69ac78a403a53eaae3e23378f3423b32169
Bobby <bobbysebolao@gmail.com>
2019-09-24 23:55:52 +0300

    Create router.test.js

---
+++ src/__tests__/router.test.js
@@ -0,0 +1,9 @@
+const readingTimeCalculator = require("../readingTimeCalculator.js");
+
+it('Testing to see if Jest works', () => {
+    expect(2).toBe(2)
+  })
+
+  test('Calculates avg time to read a chunk of text', () => {
+    expect(readingTimeCalculator("how now brown cow")).toBe(0);
+  });
