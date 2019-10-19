f38e2e708decf4f5edb763d1c24f1d17d37e663b
Bobby <bobbysebolao@gmail.com>
2019-10-19 23:16:05 +0300

    Add night mode toggle to work page

--- public/scripts/global/nightMode.js
+++ public/scripts/global/nightMode.js
@@ -12,7 +12,20 @@ nightMode.addEventListener("click", () => {
   if (standardStylesheet.href.match("/css/blog.css")) {
     standardStylesheet.href = "/css/night.css";
     sessionStorage.setItem("autosave", standardStylesheet.href);
-  } else {
+  } 
+  else if (standardStylesheet.href.match("/css/night.css")){
+    standardStylesheet.href = "/css/blog.css";
+    sessionStorage.removeItem("autosave");
+  }
+  else if (standardStylesheet.href.match("/css/work.css")){
+    standardStylesheet.href = "/css/work-night.css";
+    sessionStorage.setItem("autosave", standardStylesheet.href);
+  }
+  else if (standardStylesheet.href.match("/css/work-night.css")){
+    standardStylesheet.href = "/css/work.css";
+    sessionStorage.removeItem("autosave");
+  }
+  else {
     standardStylesheet.href = "/css/blog.css";
     sessionStorage.removeItem("autosave");
   }
