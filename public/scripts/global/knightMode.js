5d0aa214d0b90c7e5c54917ed7066dbea801c95a
Bobby <bobbysebolao@gmail.com>
2019-11-23 12:01:48 +0000

    Apply knight mode background image to multiple pages

--- public/scripts/global/knightMode.js
+++ public/scripts/global/knightMode.js
@@ -2,14 +2,14 @@ console.log("knights ahoy");
 
 let knightMode = document.querySelector(".knightMode");
 
-if (typeof standardStylesheet === 'undefined') {
-let standardStylesheet = document.querySelector("#standardStylesheet");
+if (typeof standardStylesheet === "undefined") {
+  let standardStylesheet = document.querySelector("#standardStylesheet");
 }
 
 //ACTIVATING KNIGHT mode
 
 knightMode.addEventListener("click", () => {
-  console.log("Tyler", standardStylesheet)
+  console.log("Tyler", standardStylesheet);
 
   let homeTitleWordOne = document.querySelector(".home-title__wordone");
   let homeTitleWordTwo = document.querySelector(".home-title__wordtwo");
@@ -20,37 +20,31 @@ knightMode.addEventListener("click", () => {
   if (standardStylesheet.href.match("/css/blog.css")) {
     standardStylesheet.href = "/css/knight.css";
     sessionStorage.setItem("autosave", standardStylesheet.href);
-  } 
-  else if (standardStylesheet.href.match("/css/knight.css")){
+  } else if (standardStylesheet.href.match("/css/knight.css")) {
     standardStylesheet.href = "/css/blog.css";
     sessionStorage.removeItem("autosave");
-  }
-  else if (standardStylesheet.href.match("/css/work.css")){
+  } else if (standardStylesheet.href.match("/css/work.css")) {
     standardStylesheet.href = "/css/work-knight.css";
     sessionStorage.setItem("autosave", standardStylesheet.href);
-  }
-  else if (standardStylesheet.href.match("/css/work-knight.css")){
+  } else if (standardStylesheet.href.match("/css/work-knight.css")) {
     standardStylesheet.href = "/css/work.css";
     sessionStorage.removeItem("autosave");
-  }
-  else if (standardStylesheet.href.match("/css/about.css")){
+  } else if (standardStylesheet.href.match("/css/about.css")) {
     standardStylesheet.href = "/css/about-knight.css";
     sessionStorage.setItem("autosave", standardStylesheet.href);
-  }
-  else if (standardStylesheet.href.match("/css/about-knight.css")){
+  } else if (standardStylesheet.href.match("/css/about-knight.css")) {
     standardStylesheet.href = "/css/about.css";
     sessionStorage.removeItem("autosave");
-  }
-  else if (standardStylesheet.href.match("/css/home.css")){
+  } else if (standardStylesheet.href.match("/css/home.css")) {
     standardStylesheet.href = "/css/home-knight.css";
     homeTitleWordOne.textContent = "Hail ";
     homeTitleWordTwo.textContent = " 'Tis";
     homeTitleWordThree.textContent = "Bobby";
 
-    homeSubtitle.textContent = "My surname means 'big onion' in Portuguese";
+    homeSubtitle.textContent =
+      "My surname is a homonym for 'big onion' in Portuguese";
     sessionStorage.setItem("autosave", standardStylesheet.href);
-  }
-  else if (standardStylesheet.href.match("/css/home-knight.css")){
+  } else if (standardStylesheet.href.match("/css/home-knight.css")) {
     homeTitleWordOne.textContent = "Hi ";
     homeTitleWordTwo.textContent = " I'm";
     homeTitleWordThree.textContent = "Bobby";
@@ -58,8 +52,7 @@ knightMode.addEventListener("click", () => {
     homeSubtitle.textContent = "I tell stories with prose and code";
     standardStylesheet.href = "/css/home.css";
     sessionStorage.removeItem("autosave");
-  }
-  else {
+  } else {
     standardStylesheet.href = "/css/blog.css";
     sessionStorage.removeItem("autosave");
   }
